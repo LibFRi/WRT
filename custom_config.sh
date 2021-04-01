f53cd44d43d66ee1c751fa98a352bc5f291d05a5
LibFRi <libfrichar@gmail.com>
2021-04-01 19:15:23 +0800

    init

---
+++ custom_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+#
+# File name: custom_config.sh
+# Description: Add custom config
+#
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
---
+++ custom_feeds.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# 
+# File name: custom_feeds.sh
+# Description: Add a feed source
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
