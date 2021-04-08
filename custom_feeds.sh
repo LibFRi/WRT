400835e99abfacef0c7a481055fdb88173aec55b
LibFRi <libfrichar@gmail.com>
2021-04-08 16:54:39 +0800

    [UPD] ttyd, sqm

--- custom_feeds.sh
+++ custom_feeds.sh
@@ -15,4 +15,6 @@
 
 # Add a feed source
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
+git clone -b master --depth 1 https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
 git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
