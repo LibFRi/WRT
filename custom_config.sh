5d76db8bc65b124ac30d20650fce8c8be8bb7db0
Librarian <libfrichard@gmail.com>
2021-04-19 12:26:39 +0800

    Update custom_config.sh

--- custom_config.sh
+++ custom_config.sh
@@ -10,5 +10,8 @@
 # Description: Add custom config
 #
 
+# Add UPX to staging_dir/host/bin
+wget -q -O - https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
+tar -Jx --strip 1 -f - -C staging_dir/host/bin upx-3.96-amd64_linux/upx
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
