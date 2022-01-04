1a14003b9ce0cda9184a382ecb942c2f5b810008
LibFRi <libfrichar@gmail.com>
2022-01-04 13:23:25 +0800

    remove some package

--- custom_feeds.sh
+++ custom_feeds.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 
+# Copyright (c) 2019-2020
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# 
+#
 # File name: custom_feeds.sh
 # Description: Add a feed source
 #
@@ -15,6 +15,3 @@
 
 # Add a feed source
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
-git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
-git clone -b master --depth 1 https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
-git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
